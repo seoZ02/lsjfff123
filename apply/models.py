c2491e6b7280deda2760c49ea8cd4bde24709b21
이서정 <lsjffff@gmail.com>
2021-07-23 23:14:22 +0900

    first commit

---
+++ apply/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Apply(models.Model):
+    name = models.CharField(max_length=10)
+    snum = models.CharField(max_length=10)
+    dept = models.CharField(max_length=20)
+    motive = models.TextField(max_length=500)
+    service = models.TextField(max_length=500)
+    saw = models.TextField(max_length=500)
+    aspire = models.TextField(max_length=500)
+    time = models.DateField()
